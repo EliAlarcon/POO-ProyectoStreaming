62772ae62fc3966d3c93481bfef036fc76cb1a8d
EliAlarcon <EliAlarcon@users.noreply.github.com>
2023-08-28 06:28:29 -0500

    Actualizacion4

--- src/main/java/com/alarcon/springstreming/controladores/CatalogoController.java
+++ src/main/java/com/alarcon/springstreming/controladores/CatalogoController.java
@@ -1,7 +1,13 @@
 package com.alarcon.springstreming.controladores;
 
+import com.alarcon.springstreming.repositorios.UsuarioRepository;
+import com.alarcon.springstreming.usuarios.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CatalogoController {
--- src/main/java/com/alarcon/springstreming/controladores/PrincipalController.java
+++ src/main/java/com/alarcon/springstreming/controladores/PrincipalController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Principal {
+public class PrincipalController {
 
     @GetMapping("/")
     public String home(){
